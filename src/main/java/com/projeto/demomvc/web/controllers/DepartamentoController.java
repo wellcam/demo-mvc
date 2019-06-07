d6610415c411cbb4105d456ba318a959b51cf761
Wellington Camilo <wellington.camilo@discover.com.br>
2019-06-07 11:06:57 -0300

    Add salvar controller Departamento

--- src/main/java/com/projeto/demomvc/web/controllers/DepartamentoController.java
+++ src/main/java/com/projeto/demomvc/web/controllers/DepartamentoController.java
@@ -1,15 +1,23 @@
 package com.projeto.demomvc.web.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.projeto.demomvc.domain.Departamento;
+import com.projeto.demomvc.service.DepartamentoService;
 
 @Controller
 @RequestMapping("/departamentos")
 public class DepartamentoController {
 	
+	@Autowired
+	private DepartamentoService departamentoService;
+	
 	@GetMapping("/cadastrar")
-	public String cadastrar() {
+	public String cadastrar(Departamento departamento) {
 		
 		return "/departamento/cadastro";
 	}
@@ -19,5 +27,16 @@ public class DepartamentoController {
 		
 		return "/departamento/lista";
 	}
+	
+	@PostMapping("salvar")
+	public String salvar(Departamento departamento) {
+		getDepService().salvar(departamento);
+		
+		return "redirect:/departamentos/cadastrar";
+	}
 
+	public DepartamentoService getDepService() {
+		return departamentoService;
+	}
+	
 }
