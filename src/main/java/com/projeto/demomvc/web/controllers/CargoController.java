d4e762d46eb75cf27f2aa56467b554283bb350a8
Wellington Camilo <wellington.camilo@discover.com.br>
2019-06-07 14:10:50 -0300

    Metodo de salvar nas controllers

--- src/main/java/com/projeto/demomvc/web/controllers/CargoController.java
+++ src/main/java/com/projeto/demomvc/web/controllers/CargoController.java
@@ -1,16 +1,24 @@
 package com.projeto.demomvc.web.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.projeto.demomvc.domain.Cargo;
+import com.projeto.demomvc.service.CargoService;
 
 
 @Controller
 @RequestMapping("/cargos")
 public class CargoController {
+	
+	@Autowired
+	private CargoService cargoService;
 
 	@GetMapping("/cadastrar")
-	public String cadastrar() {
+	public String cadastrar(Cargo cargo) {
 
 		return "/cargo/cadastro";
 	}
@@ -20,4 +28,16 @@ public class CargoController {
 
 		return "/cargo/lista";
 	}
+	
+	@PostMapping("salvar")
+	public String salvar(Cargo cargo) {
+		getCargoService().salvar(cargo);
+		
+		return "redirect:/cargos/cadastrar";
+	}
+
+	public CargoService getCargoService() {
+		return cargoService;
+	}
+	
 }
--- src/main/java/com/projeto/demomvc/web/controllers/FuncionarioController.java
+++ src/main/java/com/projeto/demomvc/web/controllers/FuncionarioController.java
@@ -1,14 +1,22 @@
 package com.projeto.demomvc.web.controllers;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.projeto.demomvc.domain.Funcionario;
+import com.projeto.demomvc.service.FuncionarioService;
 
 @Controller
 @RequestMapping("/funcionarios")
 public class FuncionarioController {
 	
+	@Autowired
+	private FuncionarioService funcionarioService;
+	
 	@GetMapping("/cadastrar")
-	public String cadastrar() {
+	public String cadastrar(Funcionario funcionario) {
 
 		return "/funcionario/cadastro";
 	}
@@ -18,4 +26,16 @@ public class FuncionarioController {
 
 		return "/funcionario/lista";
 	}
+	
+	@PostMapping
+	public String salvar(Funcionario funcionario) {
+		getFuncionarioService().salvar(funcionario);
+		
+		return "redirect:/funcionarios/cadastrar";
+	}
+
+	public FuncionarioService getFuncionarioService() {
+		return funcionarioService;
+	}
+	
 }
